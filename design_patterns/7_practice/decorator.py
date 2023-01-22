4e6b4cd9e736760ca87b2fcfc30a88b848fec232
chelouche9 <yonatan@moveohls.com>
2023-01-22 13:57:01 +0200

    adding practice tasks

---
+++ design_patterns/7_practice/decorator.py
@@ -0,0 +1,4 @@
+# You are a software engineer in a gaming company.
+# You are tasked with creating the Character boosts decorator class.
+# A character can fight, walk and jump. 
+# If a character finds a magic bean and eats it he makes x2 damage, jump x2 higher and walk x2 faster.
---
+++ design_patterns/7_practice/state.py
@@ -0,0 +1,6 @@
+# You are a software engineer in a gaming company.
+# You are tasked with creating the Character class.
+# A character can fight, cast spell and run. 
+# A character has different states: fighting, casting, running and idle (default state).
+# A character cannot: run when fighting and cast a spell when running or walking.
+# Implement these actions based on their state restrictions and how they impact the state.
